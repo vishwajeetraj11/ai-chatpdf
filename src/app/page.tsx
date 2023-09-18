409e5a481f44237b6fd5c8cb5f5ea5b8042841b7
vishwajeet-graphy <vishwajeet.raj@graphy.com>
2023-09-19 00:08:30 +0530

    adding authentication: clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,47 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs";
+import { ArrowRight, LogIn } from "lucide-react";
+import Link from "next/link";
 
-export default function Home() {
-  return <h1 className="text-red-400">Hello world</h1>;
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = Boolean(userId);
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+
+          <div className="flex mt-2">
+            {isAuth && (
+              <Button>
+                Go to Chats <ArrowRight className="ml-2" />
+              </Button>
+            )}
+          </div>
+
+          <p className="max-w-xl mt-1 text-lg text-slate-600">
+            Join millions of students, researchers and professinals to instantly
+            anwer questions and understand research with AI
+          </p>
+
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>File upload component</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login to get Started!
+                  <LogIn className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
