56cc0e2003b527c85d47f5bf35d745ad4f947550
Vishwajeet Raj <ext-vishwajeet.raj@graphy.com>
2023-10-18 09:48:39 +0530

    ui fix for chat component.

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -45,7 +45,7 @@ const ChatComponent = ({ chatId }: Props) => {
       id="message-container"
       className="relative max-h-screen overflow-scroll"
     >
-      <div className="sticky top-0 inset-x-0 p-2 bg-whitw h-fit">
+      <div className="sticky top-0 inset-x-0 p-2 bg-whitw h-fit bg-white">
         <h3 className="text-xl font-bold">Chat</h3>
       </div>
       {/* Messages */}
